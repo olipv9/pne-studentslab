990de3cd3639d859ef0586a0b4da6e85da3656ad
olipv9 <o.pina.2023@alumnos.urjc.es>
2024-02-13 12:11:35 +0100

    .

--- P00/e2.py
+++ P00/e2.py
@@ -1,18 +1,19 @@
-# Implement the seq_read_fasta(filename) function.
-# It should open a file in FASTA format and return a String with the DNA sequence.
-# The header is removed as well as the '\n' characters.
-
-
 def seq_read_fasta(filename):
-    final = filename.split('/')
-    print('Folder:', final[1], '\nFilename:', final[2])
-    from pathlib import Path
-    file_contents = Path(filename).read_text()
-    list_content = file_contents.split('\n')
-    list_content.pop(0)
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            seq_joined = ''.join(lines[1:])
+            sequence = seq_joined[:20]
+    except FileNotFoundError:
+        print("Error: File", filename, "not found.")
+        sequence = None
+    return sequence
 
-
-seq_read_fasta('../sequences/U5_sequence.fa')
+# Example:
+filename = '../sequences/ADA_sequence.fa'
+sequence = seq_read_fasta(filename)
+print("DNA sequence:")
+print(sequence)
 
 
 
