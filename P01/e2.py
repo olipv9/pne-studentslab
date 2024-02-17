8121207ad8c598400d152e9bede4b5816cd08b42
olipv9 <o.pina.2023@alumnos.urjc.es>
2024-02-17 11:58:06 +0100

    P01

--- P01/e2.py
+++ P01/e2.py
@@ -1,17 +1,18 @@
 #The first you should do in the __init()__ method is checking if it is a null sequence.
 # If so, print the message on the console, assign the value to the self.strbases attribute and return.
 # If it is not null, continue with the other checks.
-
 class Seq:
     def __init__(self, strbases=""):
         d = ['A', 'T', 'C', 'G']
         for i in strbases:
             if i not in d:
-                self.strbases = 'Invalid sequence.'
+                self.strbases = 'ERROR!'
+                print('Invalid sequence!')
                 break
         else:
             self.strbases = strbases
-            print('A new sequence was created!')
+            if self.strbases == strbases and strbases != "":
+                print('A new sequence was created!')
 
     def __str__(self):
         return self.strbases
@@ -24,14 +25,11 @@ class Seq:
             print('Null sequence created.')
         return 'NULL' if self.strbases == '' else self.strbases
 
-
 # -- Creating a Null sequence
 s1 = Seq()
 s2 = Seq("TATAC")
-s3 = Seq("TAXXXC")
-
 
 print('Sequence 1:', s1.is_null_sequence())
 print('Sequence 2:', s2.is_null_sequence())
-print('Sequence 3:', s3.is_null_sequence())
+
 
