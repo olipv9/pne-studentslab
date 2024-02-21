62dbc8dbc542cf9bbc4c0177a3341c26dd99af9c
oliviapv <o.pina.2023@alumnos.urjc.es>
2024-02-21 17:16:30 +0100

    .

--- S07/ÑLWKTRG.py
+++ S07/ÑLWKTRG.py
@@ -1,7 +1,7 @@
 class Car:
-    def __init__(self, brand):
+    def __init__(self, brand,speed=0):
         self.brand = brand
-        self.speed = 0
+        self.speed = speed
         brand += 'TM'
 
     def set_speed(self, speed):
@@ -12,9 +12,26 @@ class Car:
         return self.speed
 
 
+class Ferrari (Car):
+    def __init__(self):
+        super().__init__('ferrari', 100)
+        # Esto lo que hace es q coge todas las características del init de arriba, de la main class,
+        # y se las añade porque como depende de la clase de arriba necesita tener los mismos
+        # atributos, después ya puedes añadir extras que sean caraterísticos de la sub-clase.
+        self.music = 'classic'
+
+    def make_cabrio(self):
+        self.speed = 20
+        self.music = 'loud'
+        return 'WOW'
+
+
 my_car = Car('renault')
-print(my_car.get_speed())
-my_car.set_speed(80)
-print(my_car.get_speed())
+yourcar = Ferrari()
+print(yourcar.brand)
+yourcar.set_speed(120)
+print(yourcar.speed)
+print(yourcar.make_cabrio(), 'and music is', yourcar.music, 'and speed', yourcar.speed)
+
 
 
