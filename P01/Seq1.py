a18d847b00b215aa679722ebeb1431110651c3ae
olipv9 <157625202+olipv9@users.noreply.github.com>
2024-02-21 17:22:21 +0100

    Seq1.py

--- P01/Seq1.py
+++ P01/Seq1.py
@@ -1,38 +1,97 @@
-
 class Seq:
-    def __init__(self, strbases):
+    def __init__(self, strbases=""):
         d = ['A', 'T', 'C', 'G']
+        self.valid_sequence = True
         for i in strbases:
-            if i in d:
-                self.strbases = strbases
-            else:
-                self.strbases = 'ERROR'
-        if self.strbases == 'ERROR':
-            print('ERROR!!')
+            if i not in d:
+                self.valid_sequence = False
+                self.strbases = 'ERROR!'
+                print('Invalid sequence!')
+                break
         else:
-            print('A new sequence was created!')
+            self.strbases = strbases
+            if self.strbases == strbases and strbases != "":
+                print('A new sequence was created!')
 
     def __str__(self):
-
         return self.strbases
 
     def len(self):
-        return len(self.strbases)
+        return '0' if self.strbases == 'ERROR!' else len(self.strbases)
 
+    def is_null_sequence(self):
+        if self.strbases == '' and self.valid_sequence:
+            print('Null sequence created.')
+        return 'NULL' if self.strbases == '' else self.strbases
 
-def print_seqs(seq_list):
-    for i in seq_list:
-        print('Sequence:', seq_list.index(i), '(Length:' + str(i.len()) + ')', i)
-
-
-def generate_seqs(pattern, number):
-    list_1 = []
-    count = 1
-    error = False
-    while not error:
-        if count <= number:
-            list_1.append(pattern * count)
-            count += 1
+    def rev_seq(self):
+        if self.strbases == 'ERROR!' or self.strbases == 'NULL':
+            reversed_seq = self.strbases
         else:
-            error = True
-    return list_1
+            reversed_seq = self.strbases[::-1]
+        return 'NULL' if reversed_seq == '' else reversed_seq
+
+    def complement(self):
+        complementary_bases = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+        final_list = []
+        for i in self.strbases:
+            if i in complementary_bases.keys():
+                final_list.append(complementary_bases[i])
+        if final_list:
+            print('\tComplementary: ', end='')
+            for i in final_list:
+                print(i, end='')
+            print(end='\n')
+        elif self.strbases == 'ERROR!':
+            print('\tComplementary: ', self.strbases)
+        else:
+            print('\tComplementary: NULL')
+
+    def read_fasta(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                lines = file.readlines()
+                self.strbases = ''.join(lines[1:]).replace('\n', '')
+        except FileNotFoundError:
+            print("Error: File", filename, "not found.")
+            self.strbases = None
+        return self.strbases
+
+    def count_base(self, base):
+        count_base = 0
+        for i in self.strbases:
+            if i == base:
+                count_base += 1
+        return count_base
+
+    def seq_count(self):
+        dic_bases = {'A': 0, 'G': 0, 'T': 0, 'C': 0}
+        for i in self.strbases:
+            if i == 'A':
+                dic_bases['A'] += 1
+            elif i == 'T':
+                dic_bases['T'] += 1
+            elif i == 'C':
+                dic_bases['C'] += 1
+            elif i == 'G':
+                dic_bases['G'] += 1
+        return dic_bases
+
+    def most_freq_base(self):
+        dic_bases = {}
+        highest = 0
+        name = ''
+        for i in self.strbases.upper():
+            if i == 'A' or i == 'G' or i == 'T' or i == 'C':
+                if i in dic_bases:
+                    dic_bases[i] += 1
+                else:
+                    dic_bases[i] = 1
+
+        for key, val in dic_bases.items():
+            if val > highest:
+                highest = val
+                name = key
+        return name
+
+
