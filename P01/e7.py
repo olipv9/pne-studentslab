b9ac1f00919d42b1e99adef04efa53b4fbfe7dc1
olipv9 <157625202+olipv9@users.noreply.github.com>
2024-02-21 17:54:55 +0100

    Update e7.py

--- P01/e7.py
+++ P01/e7.py
@@ -5,9 +5,11 @@ s1 = Seq()
 s2 = Seq("TATAC")
 s3 = Seq("TAXXXC")
 
-print('Sequence 1:', '(Length:' + str(s1.len()) + ')', s1.is_null_sequence())
+print('Sequence 1:', '(Length:' + str(s1.len()) + ')', s1)
 print('Reversed:', s1.rev_seq())
-print('Sequence 2:', '(Length:' + str(s2.len()) + ')', s2.is_null_sequence())
+print('Sequence 2:', '(Length:' + str(s2.len()) + ')', s2)
 print('Reversed:', s2.rev_seq())
-print('Sequence 3:', '(Length:' + str(s3.len()) + ')', s3.is_null_sequence())
+print('Sequence 3:', '(Length:' + str(s3.len()) + ')', s3)
 print('Reversed:', s3.rev_seq())
+
+
