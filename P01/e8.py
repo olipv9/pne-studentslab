9ad3b1a3ff2fc27d6d885d98eb6ac8f169152e7e
olipv9 <157625202+olipv9@users.noreply.github.com>
2024-02-21 17:55:19 +0100

    Update e8.py

--- P01/e8.py
+++ P01/e8.py
@@ -4,14 +4,14 @@ s1 = Seq()
 s2 = Seq("TATAC")
 s3 = Seq("TAXXXC")
 
-print('Sequence 1:', '(Length:' + str(s1.len()) + ')', s1.is_null_sequence())
+print('Sequence 1:', '(Length:' + str(s1.len()) + ')', s1)
 print('\tReversed:', s1.rev_seq())
 s1.complement()
 
-print('Sequence 2:', '(Length:' + str(s2.len()) + ')', s2.is_null_sequence())
+print('Sequence 2:', '(Length:' + str(s2.len()) + ')', s2)
 print('\tReversed:', s2.rev_seq())
 s2.complement()
 
-print('Sequence 3:', '(Length:' + str(s3.len()) + ')', s3.is_null_sequence())
+print('Sequence 3:', '(Length:' + str(s3.len()) + ')', s3)
 print('\tReversed:', s3.rev_seq())
 s3.complement()
