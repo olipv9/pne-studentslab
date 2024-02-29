afba53925fc4b72ea6ef8eaed01665d6b094cb0c
oliviapv <o.pina.2023@alumnos.urjc.es>
2024-02-29 15:57:14 +0100

    .

--- P02/e4.py
+++ P02/e4.py
@@ -1,13 +1,17 @@
 from Seq1 import Seq
 from Client0 import Client
 IP = "212.128.255.75"
-PORT = 8081
+PORT = 8080
 s = Seq()
 c = Client(IP, PORT)
 message = s.read_fasta('../sequences/U5_sequence.fa')
 print('To server: U5 gene seq')
-c.talk(message)
+print('From server: ')
+c.talk('Sending U5 Gene to the server...')
 print(f'To server: {message}')
+print('From server: ')
+c.talk(message)
+
 
 
 
