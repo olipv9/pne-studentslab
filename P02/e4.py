0d3e19cd063417cc456ef5c4cd2223d2fd9e5d7d
oliviapv <o.pina.2023@alumnos.urjc.es>
2024-03-05 15:10:10 +0100

    .

--- P02/e4.py
+++ P02/e4.py
@@ -3,17 +3,25 @@ from Client0 import Client
 import termcolor
 IP = "212.128.255.75"
 PORT = 8081
-s = Seq()
+s1 = Seq()
+s2 = Seq()
+s3 = Seq()
 c = Client(IP, PORT)
-s.read_fasta('../sequences/U5_sequence.fa')
-print('To server:', end=' ')
-termcolor.cprint('Sending U5 Gene to the server...', 'blue')
-print('From server:\n')
-termcolor.cprint(c.talk('Sending U5 Gene to the server...'), 'green')
-print('To server:', end=' ')
-termcolor.cprint(str(s), 'blue')
-print('From server: \n ')
-termcolor.cprint(c.talk(str(s)), 'green')
+
+
+filenames = [s1.read_fasta('../sequences/U5_sequence.fa'), s2.read_fasta('../sequences/ADA_sequence.fa'), s3.read_fasta('../sequences/FRAT1_sequence.fa')]
+final_name_list = ['U5', 'ADA', 'FRAT1']
+
+for i in final_name_list:
+    index = final_name_list.index(i)
+    print('To server:', end=' ')
+    termcolor.cprint(f'Sending {i} Gene to the server...', 'blue')
+    print('From server:\n')
+    termcolor.cprint(c.talk(f'Sending {i} Gene to the server...'), 'green')
+    print('To server:', end=' ')
+    termcolor.cprint(str(filenames[index]), 'blue')
+    print('From server: \n ')
+    termcolor.cprint(c.talk(str(filenames[index])), 'green')
 
 
 
