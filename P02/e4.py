24bc97c1749e830ef3b27bedde2f90214ee49fef
oliviapv <o.pina.2023@alumnos.urjc.es>
2024-03-05 14:52:43 +0100

    .

--- P02/e4.py
+++ P02/e4.py
@@ -1,19 +1,19 @@
 from Seq1 import Seq
 from Client0 import Client
 import termcolor
-IP = "212.128.255.75"
+IP = "212.128.255.39"
 PORT = 8081
 s = Seq()
 c = Client(IP, PORT)
-message = s.read_fasta('../sequences/U5_sequence.fa')
+s.read_fasta('../sequences/U5_sequence.fa')
 print('To server:', end=' ')
-termcolor.cprint('Sending U5 Gene to the server...','blue')
+termcolor.cprint('Sending U5 Gene to the server...', 'blue')
 print('From server:\n')
 termcolor.cprint(c.talk('Sending U5 Gene to the server...'), 'green')
 print('To server:', end=' ')
-termcolor.cprint(message, 'blue')
+termcolor.cprint(str(s), 'blue')
 print('From server: \n ')
-termcolor.cprint(c.talk(message), 'green')
+termcolor.cprint(c.talk(str(s)), 'green')
 
 
 
