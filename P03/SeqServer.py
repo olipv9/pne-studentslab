66c4c17658cdaa07365ed47e105d6cfb116d3436
olipv9 <o.pina.2023@alumnos.urjc.es>
2024-03-12 08:55:29 +0100

    ultimating P03- errors taken into account

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -40,13 +40,13 @@ ls.listen()
 print("The server is configured!")
 
 while True:
-    # -- Waits for a client to connect
+    # Waits for a client to connect
     print("Waiting for Clients to connect")
 
     try:
         (cs, client_ip_port) = ls.accept()
 
-    # -- Server stopped manually
+    # Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
         # -- Close the listening socket
@@ -56,7 +56,7 @@ while True:
         print('There has been a problem with the socket.')
         ls.close()
         exit()
-    # -- Execute this part if there are no errors
+    # Execute this part only if there are no errors
     else:
         print("A client has connected to the server!")
         msg_raw = cs.recv(2048)
