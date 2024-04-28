40953e6882a5147e3e83652b2dac29fabd058958
olipv9 <o.pina.2023@alumnos.urjc.es>
2024-04-28 20:19:51 +0200

    p7

--- P07/e2.py
+++ P07/e2.py
@@ -17,28 +17,23 @@ gene_identifiers = {}
 
 # Iterate over genes and make requests for each gene
 for gene in genes:
-    # Establish connection with the server
-    connection = http.client.HTTPSConnection(SERVER)
 
     try:
-        # Send GET request to Ensembl REST API endpoint for the gene
+        # Establish connection with the server, and send GET request to Ensembl REST API endpoint for the gene
+        connection = http.client.HTTPSConnection(SERVER)
         connection.request("GET", ENDPOINT + gene + "?content-type=application/json")
 
         # Get the response
         response = connection.getresponse()
-
-        # Read and parse the JSON response
         data = json.loads(response.read().decode('utf-8'))
 
-        # Check if the request was successful (status code 200)
+        # Check if the request:
         if response.status == 200:
-            # Extract the stable identifier (gene id) from the first result
+            # Obtain the stable identifier (gene id) from the dict they offer:
             gene_id = data[0]["id"]
-
-            # Store the gene identifier in the dictionary
             gene_identifiers[gene] = gene_id
         else:
-            print(f"Error: {response.status} - {response.reason} for gene {gene}")
+            print(f"Error: {response.status} - {response.reason}: gene {gene}")
 
     except Exception as e:
         print(f"An error occurred: gene {gene} -> {e}")
@@ -47,7 +42,7 @@ for gene in genes:
         # Close the connection
         connection.close()
 
-# Print gene identifiers
+
 for gene, s_identifier in gene_identifiers.items():
     termcolor.cprint(f'{gene}: ', 'green', end='')
     print(f'-----> {s_identifier}')
