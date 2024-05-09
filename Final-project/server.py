a15c493362edf08e69050981d66e115e1250d2ed
olipv9 <o.pina.2023@alumnos.urjc.es>
2024-05-09 23:59:30 +0200

    p8

--- Final-project/server.py
+++ Final-project/server.py
@@ -2,30 +2,40 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
-import jinja2 as j
-from useful_function import check_for_seq_errors, read_html_file
+import os
 
 # Define the Server's port
 PORT = 8080
-num = 200
+
+
 # -- This is for preventing the error: "Port already in use"
-socketserver.TCPServer.allow_reuse_address = True
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
-    def get_webpage_main(self):
+    def do_GET(self):
 
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
+
         # Open the form1.html file
         # Read the index from the file
         try:
-            if self.requestline.split(' ')[1] == '/':
+            request_path = self.requestline.split(' ')[1]
+            if request_path == '/':
                 body = Path('html/index.html').read_text()
-
-        except (IndexError, FileNotFoundError):
+            elif request_path.startswith('/number_1'):
+                body = Path('html/species_num.html').read_text()
+            elif request_path.startswith('/number_2'):
+                body = Path('html/karyotype.html').read_text()
+            elif request_path.startswith('/number_3'):
+                if request_path.split('=')[1].startswith('&') or request_path.split('=')[-1] == '':
+                    body = Path('html/specific_error.html').read_text()
+                else:
+                    body = Path('html/chrom_len.html').read_text()
+            else:
+                body = Path('html/error.html').read_text()
+        except FileNotFoundError:
             body = Path('html/error.html').read_text()
-
 
         # Generating the response message
 
@@ -33,7 +43,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(body.encode()))
+        self.send_header('Content-Length', str(len(body)))  # Length of body as string
 
         # The header is finished
         self.end_headers()
@@ -48,7 +58,6 @@ Handler = TestHandler
 
 # -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-
     print("Serving at PORT", PORT)
 
     # -- Main loop: Attend the client. Whenever there is a new
---
+++ P06/tryouts.py
@@ -0,0 +1,3 @@
+
+msg = 'http://127.0.0.1:8080/number_1?msg=pco'.split('/')[3].split('?')[0]
+print(msg)
